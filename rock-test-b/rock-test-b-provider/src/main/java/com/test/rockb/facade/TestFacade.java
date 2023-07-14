36aa3bc6ef9bc226c8dfd1b9e4e0360efcb9b21b
rockManFlow <1670903022@qq.com>
2023-07-14 16:03:21 +0800

    Delete rock-test-b directory

--- rock-test-b/rock-test-b-provider/src/main/java/com/test/rockb/facade/TestFacade.java
+++
@@ -1,8 +0,0 @@
-package com.test.rockb.facade;
-
-import com.test.rockb.request.TestRequest;
-import com.test.rockb.response.TestResponse;
-
-public interface TestFacade {
-    public TestResponse create(TestRequest request);
-}
